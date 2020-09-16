ba92df604759e49ecef57f31ca1caad113465cbd
Darshan R <darshandarsh27blr@gmail.com>
2020-09-16 13:56:58 +0530

    Added solution to code_3

---
+++ code solutions/code_3.c
@@ -0,0 +1,19 @@
+// Swapping the values without using a temporary variable
+
+#include <stdio.h>
+int main()
+{
+    int first, second;
+    printf("Enter the first number:");
+    scanf("%d", &first);
+    printf("\nEnter the second numbr:");
+    scanf("%d", &second);
+
+    // logic
+    first = first - second;
+    second = first + second;
+    first = second - first;
+
+    printf("After swapping\nFirst number:%d\nSecond number:%d", first, second);
+    return 0;
+}
